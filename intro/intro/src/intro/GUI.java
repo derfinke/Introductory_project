ddb09baac6a294a0efb0bca744a433b99286268c
Kevin <kevin.karger@htwg-konstanz.de>
2022-12-12 16:42:45 +0100

    more buttons added

--- intro/intro/src/intro/GUI.java
+++ intro/intro/src/intro/GUI.java
@@ -13,13 +13,20 @@ public class GUI extends JFrame {
 	/*
 	 * define control buttons
 	 */
-	private JButton sa_Door_OPEN   = new JButton("OPEN");
-	private JButton sa_Door_CLOSE  = new JButton("CLOSE");
+	private JButton sa_RESET         = new JButton("RESET");
+	private JButton sa_Door_OPEN     = new JButton("OPEN");
+	private JButton sa_Door_CLOSE    = new JButton("CLOSE");
+	private JButton sa_Motor_UP_V1   = new JButton("UP V1");
+	private JButton sa_Motor_UP_V2   = new JButton("UP V2");
+	private JButton sa_Motor_DOWN_V1 = new JButton("DOWN V1");
+	private JButton sa_Motor_DOWN_V2 = new JButton("DOWN V2");
 	
 	/*
 	 * define gui labels
 	 */
 	private JLabel  sa_Door_LABEL  = new JLabel("Door Control");
+	private JLabel  sa_Motor_LABEL = new JLabel("Motor Control");
+
 	
 	/*
 	 * create gui
@@ -27,7 +34,7 @@ public class GUI extends JFrame {
 public GUI() {
 
 	setTitle("Elevator HMI Group D");
-    setSize(800,400);
+    setSize(800,300);
     setLocationRelativeTo(null);
     setLayout(null);    
     setResizable(false);
@@ -39,27 +46,55 @@ private void sa_add_constraints() {
 	/*
 	 * Label & button constraints
 	 */
+	// Reset
+	sa_RESET.setBounds(630,200, 100,40);
+	add(sa_RESET);
+
+	// Door
 	sa_Door_LABEL.setBounds(10,10, 200,40);
-	sa_Door_OPEN.setBounds(220,60, 200,40);
-	sa_Door_CLOSE.setBounds(10,60, 200,40);
-	
-	/*
-	 * add constraints to gui
-	 */
 	add(sa_Door_LABEL);
+
+	sa_Door_OPEN.setBounds(110,60, 100,40);
 	add(sa_Door_OPEN);
+
+	sa_Door_CLOSE.setBounds(10,60, 100,40);
 	add(sa_Door_CLOSE);
+
+	// Motor 
+	sa_Motor_LABEL.setBounds(330,10, 200,40);
+	add(sa_Motor_LABEL);
+
+	// Motor UP
+	sa_Motor_UP_V1.setBounds(330,60, 100,40);
+	add(sa_Motor_UP_V1);
+	
+	sa_Motor_UP_V2.setBounds(430,60, 100,40);
+	add(sa_Motor_UP_V2);
+
+	// Motor Down
+	sa_Motor_DOWN_V1.setBounds(530,60, 100,40);
+	add(sa_Motor_DOWN_V1);
+
+	sa_Motor_DOWN_V2.setBounds(630,60, 100,40);
+	add(sa_Motor_DOWN_V2);
+
 }
 
 /*
- * create constraints event handler
+ * create event handler
  */
 private void sa_constraints_ctrl_handler() {
-	
+
 	// event close the window
 	this.addWindowListener(new WindowAdapter() {
 		public void windowClosing(WindowEvent e) {
 			System.exit(1);
+		}
+	});
+	// event click to RESET the simulation
+	sa_RESET.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			// TO-DO create function or hook to reset the simuation
 		}
 	});
 	// event click to open the door
@@ -72,6 +107,30 @@ private void sa_constraints_ctrl_handler() {
 	sa_Door_CLOSE.addActionListener(new ActionListener() {
 		public void actionPerformed(ActionEvent e) {
 			// TO-DO: create function or hook to close the door
+		}
+	});
+	// event click to send the elevator upwards with v1
+	sa_Motor_UP_V1.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			// TO-DO create function or hook to send the elevator upwards with v1
+		}
+	});
+	// event click to send the elevator upwards with v2
+	sa_Motor_UP_V2.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			// TO-DO: create function or hook to send the elevator upwards with v2
+		}
+	});
+	// event click to send the elevator downwards with v1
+	sa_Motor_DOWN_V1.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			// TO-DO: create function or hook to send the elevator downwards with v1
+		}
+	});
+	// event click to send the elevator downwards with v2
+	sa_Motor_DOWN_V2.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			// TO-DO: create function or hook to send the elevator downwards with v2
 		}
 	});
 }
