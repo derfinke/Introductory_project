e26f56110205b813756ef1b8903f0ab560e445c9
finkbeiner <nicofinkbeiner@gmail.com>
2022-12-12 22:47:58 +0100

    added closeDoor and openDoor method

--- intro/intro/src/intro/ElevatorControl.java
+++ intro/intro/src/intro/ElevatorControl.java
@@ -9,46 +9,33 @@ import de.re.easymodbus.modbusclient.ModbusClient;
 public class ElevatorControl {
 	
 	private ModbusClient client;
+	private boolean DoorIsOpen;
+	private boolean DoorIsClosed;
+	private boolean MotorIsReady;
+	private boolean MotorIsOn;
+	private boolean ErrorState;
+
+	private boolean[] readValues = new boolean[5]; 
 
 	public ElevatorControl() throws UnknownHostException, IOException {
 		client = new ModbusClient("ea-pc111.ei.htwg-konstanz.de",505);
 		client.Connect();
 	}
-
-	public void run() {
-
-		openDoor();
-	}
-
-	public static void main(String[] args) {
-		ElevatorControl runner;
-		try {
-			runner = new ElevatorControl();
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-			return;
-		} catch (IOException e) {
-			e.printStackTrace();
-			return;
-		}
-		runner.run();
-	}
 	
 	public void reset() 
 	{
 		try {
-			if(!client.ReadCoils(0, 1)[0])
+			readSensor();
+			if(ErrorState)
 			{
 				client.WriteSingleCoil(0, true);
-				Thread.sleep(500);
-				client.WriteSingleCoil(0, false);
-				Thread.sleep(200);
 			}
 			else
 			{
 				client.WriteSingleCoil(0, false);
+				System.out.println("No reset needed");
 			}
-		} catch (ModbusException | IOException | InterruptedException e) {
+		} catch (ModbusException | IOException e) {
 			e.printStackTrace();
 		}
 	}
@@ -56,10 +43,58 @@ public class ElevatorControl {
 	public void openDoor() 
 	{
 		try {
+			readSensor();
 			
-			client.WriteSingleCoil(12, false);
+			if(DoorIsClosed && MotorIsReady && !MotorIsOn)
+			{
+				client.WriteSingleCoil(12, false); //set register to close door to false
+				client.WriteSingleCoil(13, true);  //set register to open door to true
+			}
+			else
+			{
+				System.out.println("Door cant be opened at the moment");
+			}
 		} catch (ModbusException | IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public void closeDoor()
+	{
+		try {
+			readSensor();
+			
+			if(DoorIsOpen && !MotorIsReady && !MotorIsOn)
+			{
+				client.WriteSingleCoil(13, false); //set register to open door to false
+				client.WriteSingleCoil(12, true);  //set register to close door to true
+			}
+			else
+			{
+				System.out.println("Door cant be closed at the moment");
+			}
+
+		} catch (ModbusException | IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void readSensor()
+	{
+		try {
+			readValues = client.ReadDiscreteInputs(80, 5); //read values from register 10.0 - 10.4
+			
+			DoorIsOpen = readValues[0];
+			DoorIsClosed = readValues[1];
+			
+			MotorIsReady = readValues[2];
+			MotorIsOn = readValues[3];
+			
+			ErrorState = readValues[4];
+			
+		} catch (ModbusException | IOException e) {
+			e.printStackTrace();
+		}
+		
 	}
 }
